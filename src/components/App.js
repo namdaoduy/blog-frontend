c02b4f96289f9a9e086ef70c9ca18608d3b80d33
nam-gotit <lenkc98@gmail.com>
2018-12-26 15:31:49 +0700

    Add header

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import CssBaseline from '@material-ui/core/CssBaseline';
+import JssProvider from 'react-jss/lib/JssProvider';
+import { create } from 'jss';
+import { createGenerateClassName, jssPreset } from '@material-ui/core/styles';
 
 import Home from './Home'
 import history from 'utils/history';
+
+const generateClassName = createGenerateClassName();
+const jss = create({
+  ...jssPreset(),
+  insertionPoint: 'jss-insertion-point',
+});
 
 export default class App extends Component {
   constructor(props) {
@@ -33,14 +42,16 @@ export default class App extends Component {
 
   render() {
     return (
-      <React.Fragment>
-        <CssBaseline />
-        <Router>
-          <Switch>
-            {this.renderRoutes()}
-          </Switch>
-        </Router>
-      </React.Fragment>
+      <JssProvider jss={jss} generateClassName={generateClassName}>
+        <React.Fragment>
+          <CssBaseline />
+          <Router>
+            <Switch>
+              {this.renderRoutes()}
+            </Switch>
+          </Router>
+        </React.Fragment>
+      </JssProvider>
     );
   }
 }
--- src/components/Common/Header.js
+++ src/components/Common/Header.js
@@ -1,10 +1,55 @@
 import React, { Component } from 'react'
-import 'assets/styles/common-header.css'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import Badge from '@material-ui/core/Badge';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import { withStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MailIcon from '@material-ui/icons/Mail';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import MoreIcon from '@material-ui/icons/MoreVert';
+
+
+import './../../assets/styles/common-header.css'
+
+const classes = {};
+const isMenuOpen = true;
 
 export default class Header extends Component {
   render() {
     return(
-      <div>Header</div>
+      <AppBar position="sticky" color="default">
+        <Toolbar>
+          <div className="header-left">
+            <IconButton className="header-menu-btn" color="inherit" aria-label="Open drawer">
+              <MenuIcon />
+            </IconButton>
+          </div>
+          <div className="header-center">
+            <Typography className="header-title" variant="h5" color="inherit" noWrap>
+              Just Blog
+            </Typography>
+          </div>
+          <div className="header-right">
+            <IconButton color="inherit">
+              <Badge badgeContent={17} color="secondary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              color="inherit">
+              <AccountCircle />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
     )
   }
 }
